0066cb5b4078fd70267322020cee9400bd43020c
scoomp <moshnyakov.alexei@yandex.ru>
2023-06-18 10:14:40 +0300

    homework 7.3

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -3,9 +3,7 @@ import s from './App.module.css'
 import HW1 from '../s2-homeworks/hw01/HW1'
 import HW5 from "../s2-homeworks/hw05/HW5";
 import HW6 from "../s2-homeworks/hw06/HW6";
-import HW7 from "../s2-homeworks/hw07/HW7";
-import HW2 from "../s2-homeworks/hw02/HW2";
-import HW8 from "../s2-homeworks/hw08/HW8";
+import {HW7} from "../s2-homeworks/hw07/HW7";
 
 function App() {
     return (
