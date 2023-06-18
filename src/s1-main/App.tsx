23f5da14de56e6fac5d598437333d988724b87d6
scoomp <moshnyakov.alexei@yandex.ru>
2023-06-18 08:16:38 +0300

    homework 8

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -5,6 +5,7 @@ import HW5 from "../s2-homeworks/hw05/HW5";
 import HW6 from "../s2-homeworks/hw06/HW6";
 import HW7 from "../s2-homeworks/hw07/HW7";
 import HW2 from "../s2-homeworks/hw02/HW2";
+import HW8 from "../s2-homeworks/hw08/HW8";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <HW5/>
             <HW6/>
             <HW7/>
+            <HW8/>
         </div>
     )
 }
