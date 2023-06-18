ea54b70b891a1641cb56238c38fa9108bd408e66
scoomp <moshnyakov.alexei@yandex.ru>
2023-06-18 11:35:24 +0300

    homework 7.10

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import s from './App.module.css'
 import HW5 from "../s2-homeworks/hw05/HW5";
-import HW6 from "../s2-homeworks/hw06/HW6";
-import HW7 from "../s2-homeworks/hw07/HW7";
+
 
 function App() {
     return (
         <div className={s.App}>
-            {/*<HW5/>
-            <HW6/>*/}
-            <HW7/>
+            <HW5/>
+
 
         </div>
     )
