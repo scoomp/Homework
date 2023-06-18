46edcb30606fbed398efa631253ba0aa399e3951
scoomp <moshnyakov.alexei@yandex.ru>
2023-06-18 11:46:36 +0300

    homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,19 +1,20 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+   | { type: 'sort'; payload: 'up' | 'down' }
+   | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-            const sortedState = [...state]
-            return sortedState.sort((a,b) => action.payload === 'up' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name))
-        }
-        case 'check': {
-            return state.filter(el => el.age >= action.payload) // need to fix
-        }
-        default:
-            return state
-    }
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+   switch (action.type) {
+      case 'sort': {
+         return [...state].sort((a, b) => action.payload === 'up'
+            ? a.name.localeCompare(b.name)
+            : b.name.localeCompare(a.name))
+      }
+      case 'check': {
+         return state.filter(user => user.age >= action.payload)
+      }
+      default:
+         return state
+   }
 }
