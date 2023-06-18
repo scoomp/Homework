0a08408b958b69f73eb46bbf82b1f5c02b7e5ca8
scoomp <moshnyakov.alexei@yandex.ru>
2023-06-18 10:38:03 +0300

    homework 7.5

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import s from './App.module.css'
-import HW1 from '../s2-homeworks/hw01/HW1'
 import HW5 from "../s2-homeworks/hw05/HW5";
 import HW6 from "../s2-homeworks/hw06/HW6";
-import {HW7} from "../s2-homeworks/hw07/HW7";
+import HW7 from "../s2-homeworks/hw07/HW7";
 
 function App() {
     return (
